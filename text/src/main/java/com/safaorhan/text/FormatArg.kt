3bde5c31023f26683368915e3cc6148416bc184a
safa <ahmetsafaorhan@gmail.com>
2022-05-17 17:10:02 +0300

    Release 1.0.7

--- text/src/main/java/com/safaorhan/text/FormatArg.kt
+++ text/src/main/java/com/safaorhan/text/FormatArg.kt
@@ -3,6 +3,6 @@ package com.safaorhan.text
 import androidx.annotation.StringRes
 
 sealed class FormatArg {
-    class Literal(val value: Any) : FormatArg()
-    class StringResource(@StringRes val id: Int) : FormatArg()
+    data class Literal(val value: Any) : FormatArg()
+    data class StringResource(@StringRes val id: Int) : FormatArg()
 }
